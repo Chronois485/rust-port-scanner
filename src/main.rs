3bf00a496c5544820155231256fe513d15d97627
Chronois485 <c98414914@gmail.com>
2026-08-02 13:05:12 +0300

    feat: add output of an errors

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,14 @@ use ports::parser;
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let ports = parser::parse_ports(
-        match args.ports {
-            Some(p) => p,
-            None => "8080".to_string(),
+    let ports = args.ports.unwrap_or_else(|| "8080".to_string());
+    let ports = match parser::parse_ports(&ports) {
+        Ok(ports) => ports,
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1);
         }
-        .as_str(),
-    );
-    let error = ports.unwrap(); // FIX LATER
-    let ports = error.as_slice();
-    let scan = scanner::scan_ports(&args.target, ports).await;
+    };
+    let scan = scanner::scan_ports(&args.target, ports.as_slice()).await;
     output::printer::print_scan_result(&scan);
 }
--- src/ports/mod.rs
+++ src/ports/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub mod parser;
 
 #[derive(Debug)]
@@ -5,4 +7,22 @@ pub enum ParsePortError {
     InvalidPort,
     InvalidRange,
     EmptyInput,
+}
+
+impl Display for ParsePortError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParsePortError::EmptyInput => {
+                write!(f, "Empty input")
+            }
+
+            ParsePortError::InvalidPort => {
+                write!(f, "Invalid port")
+            }
+
+            ParsePortError::InvalidRange => {
+                write!(f, "Invalid range")
+            }
+        }
+    }
 }
